42c6f830fcb25aada00b3d80ffe8b5d92954e168
Ꮢᴏʜɪᴛ  Mᴀʟɪᴅᴀ <rohitmalida9@gmail.com>
2026-01-13 12:30:06 +0530

    Add files via upload

---
+++ fibonacci.py
@@ -0,0 +1,13 @@
+
+def fact(n):
+    if n==0:
+        return 1
+    else:
+        return n*fact(n-1)
+
+n=int(input("enter the number"))
+    
+
+result=fact(n)
+print("factorial is",result)
+    
